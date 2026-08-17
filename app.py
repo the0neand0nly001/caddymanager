3e858e4bb5c83a7232b3cc0ce6e69ef953f5bf86
the0neand0nly001 <evansinnott9@gmail.com>
2026-08-17 22:42:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -179,6 +179,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <title>Caddy Manager</title>
+    <link rel="icon" type="image/png" href="{{ url_for('static', filename='icon.png') }}">
     <style>
         :root {
             --bg-color: #121214;
