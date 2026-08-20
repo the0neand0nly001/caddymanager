5378fc2e5be37946a040ba43de9472b9b69b1f7c
the0neand0nly001 <evansinnott9@gmail.com>
2026-08-20 20:10:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,13 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+echo "[+] Checking for git..."
+if ! command -v git &> /dev/null; then
+    echo "[+] Git not found. Installing git..."
+    apt-get update > /dev/null 2>&1
+    apt-get install -y git > /dev/null 2>&1
+fi
+
 echo "[+] Downloading Caddy Manager..."
 rm -rf /tmp/caddymanager
 git clone https://github.com/the0neand0nly001/caddymanager.git /tmp/caddymanager
